ed1892e8cfa1efaf3b34460674f0b6315a9ac733
Eric-Gona <eric.gona254@gmail.com>
2022-02-10 15:56:27 +0300

    deployment

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from app import create_app,db
 from app.models import User
 
@@ -8,7 +8,7 @@ app = create_app('production')
 manager = Manager(app)
 migrate = Migrate(app,db)
 
-manager.add_command('db',MigrateCommand)
+manager.add_command('db',Migrate)
 manager.add_command('run',Server(use_debugger=True))
 
 
